6835c1ba05128842957a3d1eb887c437e41e9c01
Pablo Fabre <pablofabre@macbook-pro-de-pablo.local>
2018-09-20 09:27:25 -0500

    Initial Commit

---
+++ 1. WebView get Youtube ID/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  1. WebView get Youtube ID
+//
+//  Created by Pablo Fabre on 9/20/18.
+//  Copyright © 2018 Pablo Fabre. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ 1. WebView get Youtube ID/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  1. WebView get Youtube ID
+//
+//  Created by Pablo Fabre on 9/20/18.
+//  Copyright © 2018 Pablo Fabre. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
